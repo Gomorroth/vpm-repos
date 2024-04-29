d9c606d8f0858aff67f1ddbd6956b737bd7225ec
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-04-29 20:44:19 +0900

    a

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,7 +1,6 @@
 const isRelease = process.env.NODE_ENV === 'production';
 
-var repoName = process.env.GITHUB_REPOSITORY ?? "vpm-repos";
-repoName = repoName.slice(repoName.indexOf("/") + 1);
+const repoName = process.env.GITHUB_REPOSITORY ? `${process.env.GITHUB_REPOSITORY.slice(process.env.GITHUB_REPOSITORY.indexOf("/"))}/` : "/vpm-repos/";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
