590ba69a149704555ff0f21010a63bbe19504238
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-04-29 20:36:05 +0900

    prefix

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,5 +1,11 @@
+const isRelease = process.env.NODE_ENV === 'production';
+
+var repoName = process.env.GITHUB_REPOSITORY ?? "vpm-repos";
+repoName = repoName.slice(repoName.indexOf("/") + 1);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isRelease ? repoName : undefined,
   reactStrictMode: true,
   output: "export",
 };
