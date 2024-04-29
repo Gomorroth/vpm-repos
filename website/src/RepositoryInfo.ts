00c3905fe7d9b15259699b941fbe10fceaa8f78d
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-04-29 19:33:06 +0900

    asd

--- website/src/RepositoryInfo.ts
+++ website/src/RepositoryInfo.ts
@@ -2,7 +2,7 @@ import { PackageInfo } from "./PackageInfo"
 
 export type RepositoryInfo = {
     name?: string,
-    author?: string,
+    author?: string | {name?: string, url?: string},
     url?: string,
     id?: string,
     packages: {
